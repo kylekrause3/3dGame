4fbdf062c3ae6e9664b42efb5dfd0a10034ebc5a
kylekrause3 <kkrause317@gmail.com>
2021-10-02 16:44:30 -0400

    Started UI Devlopment  & Health System (w/ health bars)

---
+++ Assets/Development Timeline.cs
@@ -0,0 +1,14 @@
+/* GAME WILL BE LIKE BOXHEAD, SCHOOL APPROPRIATE THOUGH
+ * fighting robots with a laser gun i guess
+ * 
+ * TODO:
+ * Health/attack system
+ * Enemies
+ * Enemy pathfinding
+ * Placing down barracades w/ health
+ * Currency system
+ * 
+ * 
+ * DONE:
+ * Masking away parts of playermodel that need to be hidden from the camera (camera culling mask)
+ */
